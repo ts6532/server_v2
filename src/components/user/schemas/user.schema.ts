7803b47eeb25c3095f5bfb5088345ae2a7379bdc
Dmitry <papkovdmitry@gmail.com>
2023-01-30 14:44:17 +0500

    reorder props in user scheme

--- src/components/user/schemas/user.schema.ts
+++ src/components/user/schemas/user.schema.ts
@@ -14,14 +14,14 @@ export class User {
   @Prop({ required: true })
   password: string;
 
+  @Prop()
+  firstname: string;
+
   @Prop({ default: false })
   isActivated: boolean;
 
   @Prop()
   activationLink: string;
-
-  @Prop()
-  firstname: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
