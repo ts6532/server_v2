df50e60d6bedc8051188fb7ada26ad761355036f
Dmitry <papkovdmitry@gmail.com>
2023-01-31 19:44:24 +0500

    update user dto's

---
+++ src/components/user/dto/update-user.dto.ts
@@ -0,0 +1,6 @@
+export class UpdateUserDto {
+  _id: string;
+  email?: string;
+  role?: string;
+  firstname?: string;
+}
--- src/components/user/dto/user_with_id.dto.ts
+++
@@ -1,8 +0,0 @@
-import { UserDto } from './user.dto';
-
-export class UserWithIdDto extends UserDto {
-  _id: string;
-  constructor() {
-    super();
-  }
-}
